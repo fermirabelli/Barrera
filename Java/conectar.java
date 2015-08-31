3a7111c5840b529a60814ed899267b81c4acbb18
fedeestape1 <estapefederico@gmail.com>
2015-08-31 17:11:40 -0300

    Update conectar.java

--- Java/conectar.java
+++ Java/conectar.java
@@ -1,5 +1,7 @@
 /*
 Este codigo genera la conexion entre la interfaz creada en java y la base de datos de mysql.
+Si o si debe ser guardada con el nombre conectar.java y guardada en la misma carpeta que la
+clase interfaz.java.
 El motor de base de datos que se utilizó específicamente para este proyecto fue phpmyadmin
 */
 package arduino;
